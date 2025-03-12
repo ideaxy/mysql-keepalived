d5d96e16de26ea3cf483fd3df31c656acf55e4ea
ideaxy <101311319+ideaxy@users.noreply.github.com>
2025-03-12 10:22:31 +0800

    Update checkMySQL.py
    使用python3和pymysql

--- keepalived/checkMySQL.py
+++ keepalived/checkMySQL.py
@@ -1,22 +1,20 @@
-#!/usr/bin/python
-#coding: utf-8
-#http://zhishuedu.com 
-#Copyriht(c) 2017 - wubx(wubx@zhishuedu.com)  
+#!/usr/bin/env python3
+# coding: utf-8
+# http://zhishuedu.com 
+# Copyright (c) 2017 - wubx(wubx@zhishuedu.com)  
 
 import sys
 import os
 import getopt
-import MySQLdb
+import pymysql
 import logging
-import filelock
+from filelock import FileLock
 import config
 
-
-dbhost=config.dbhost
-dbport=config.dbport
-dbuser=config.dbuser
-dbpassword=config.dbpassword
-
+dbhost = config.dbhost
+dbport = config.dbport
+dbuser = config.dbuser
+dbpassword = config.dbpassword
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
@@ -24,75 +22,83 @@ logging.basicConfig(level=logging.DEBUG,
                     filename='/tmp/kp_check.log',
                     filemode='a')
 
-
 def checkMySQL():
-	global dbhost
-	global dbport
-	global dbuser
-	global dbpassword
+    global dbhost
+    global dbport
+    global dbuser
+    global dbpassword
 
-	shortargs='h:P:'
-	opts, args=getopt.getopt(sys.argv[1:],shortargs)
-	for opt, value in opts:
-		if opt=='-h':
-			dbhost=value
-		elif opt=='-P':
-			dbport=value
-	#print "host : %s, port: %d, user: %s, password: %s" % (dbhost, int(dbport), dbuser, dbpassword)
-	db = instanceMySQL(dbhost, dbport, dbuser, dbpassword)
-	st = db.ishaveMySQL()
-	#if ( db.connect() != 0 ):
-	#	return 1
-	#db.disconnect()
-	return st
+    shortargs = 'h:P:'
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], shortargs)
+        for opt, value in opts:
+            if opt == '-h':
+                dbhost = value
+            elif opt == '-P':
+                dbport = value
+    except getopt.GetoptError as e:
+        logging.error(f"命令行参数解析错误: {e}")
+        return 1
+
+    db = instanceMySQL(dbhost, dbport, dbuser, dbpassword)
+    st = db.ishaveMySQL()
+    return st
 
 class instanceMySQL:
-	conn = None
-	def __init__(self, host=None,port=None, user=None, passwd=None):
-		self.dbhost= host
-		self.dbport = int(port)
-		self.dbuser = user
-		self.dbpassword = passwd
+    conn = None
 
-	def ishaveMySQL(self):
-		cmd="ps -ef | egrep -i \"mysqld\" | grep %s | egrep -iv \"mysqld_safe\" | grep -v grep | wc -l" % self.dbport
-		mysqldNum = os.popen(cmd).read()
-		cmd ="netstat -tunlp | grep \":%s\" | wc -l" % self.dbport
-		mysqlPortNum= os.popen(cmd).read()
-		#print mysqldNum, mysqlPortNum
-		if ( int(mysqldNum) <= 0):
-			print "error"
-			return 1
-		if ( int(mysqldNum) > 0 and  mysqlPortNum <= 0):
-			return 1
-		return 0
+    def __init__(self, host=None, port=None, user=None, passwd=None):
+        self.dbhost = host
+        self.dbport = int(port)
+        self.dbuser = user
+        self.dbpassword = passwd
 
-	def connect(self):
-	#	print "in db conn"
-#		print "host : %s, port: %d, user: %s, password: %s" % (self.dbhost, self.dbport, self.dbuser, self.dbpassword)
-		try:
-			self.conn=MySQLdb.connect(host="%s"%self.dbhost, port=self.dbport,user="%s"%dbuser, passwd="%s"%self.dbpassword)
-		except Exception, e:
-#			print " Error"
-			print e
-			return 1
-		return 0
-	def disconnect(self):
-		if (self.conn):
-			self.conn.close()
-			self.conn = None
+    def ishaveMySQL(self):
+        cmd = f"ps -ef | egrep -i \"mysqld\" | grep {self.dbport} | egrep -iv \"mysqld_safe\" | grep -v grep | wc -l"
+        mysqldNum = os.popen(cmd).read().strip()
+        logging.info(f"进程{self.dbport}数量:{mysqldNum}")
+        cmd = f"sudo netstat -tunlp | grep \":{self.dbport}\" | wc -l"
+        mysqlPortNum = os.popen(cmd).read().strip()
+        logging.info(f"端口{self.dbport}数量:{mysqlPortNum}")
+        try:
+            if int(mysqldNum) <= 0:
+                logging.error("进程数量小于0.")
+                return 1
+            if int(mysqldNum) > 0 and int(mysqlPortNum) <= 0:
+                logging.error("端口未被监听.")
+                return 1
+            return 0
+        except ValueError:
+            logging.error("无法将进程或端口计数转换为整数")
+            return 1
 
+    def connect(self):
+        try:
+            self.conn = pymysql.connect(
+                host=self.dbhost,
+                port=self.dbport,
+                user=dbuser,
+                password=self.dbpassword
+            )
+            return 0
+        except pymysql.Error as e:
+            logging.error(f"数据库连接失败: {e}")
+            return 1
 
-if __name__== "__main__":
-	lock = filelock.FileLock("/tmp/kpc.txt")
-	if lock:
-		logging.info("ZST Get Lock.start!!!")
-	try:
-		with lock.acquire(timeout=5):
-			pass
-	except filelock.timeout:
-		print "timeout"	        
-		logging.warning("get file lock timeout")
-		
-	st=checkMySQL()
-	sys.exit(st)
+    def disconnect(self):
+        if self.conn:
+            self.conn.close()
+            self.conn = None
+
+if __name__ == "__main__":
+    lock = FileLock("/tmp/kpc.txt")
+    if lock:
+        logging.info("Get Lock.start!!!")
+    try:
+        with lock.acquire(timeout=5):
+            pass
+    except TimeoutError:
+        logging.warning("get file lock timeout")
+
+    st = checkMySQL()
+    sys.exit(st)
