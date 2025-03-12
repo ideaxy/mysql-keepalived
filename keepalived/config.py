b5957c4031a96df18a1d501902339f108a86ff45
ideaxy <101311319+ideaxy@users.noreply.github.com>
2025-03-12 10:22:56 +0800

    Update config.py

--- keepalived/config.py
+++ keepalived/config.py
@@ -1,8 +1,4 @@
-#!/usr/bin/python
-#http://zhishuedu.com 
-#Copyriht(c) 2017 - wubx(wubx@zhishuedu.com)  
-
-dbhost='192.168.11.100'
-dbport=3309
-dbuser='wubx'
-dbpassword='wubxwubx'
+dbhost = '192.168.56.110'
+dbport = 33060
+dbuser = 'app'
+dbpassword = 'app'
